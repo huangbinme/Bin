6d1c47bbd97b20cf965d27317c441ae996e11f4b
bin <huangbinme@163.com>
2021-01-06 23:13:51 +0800

    init commit

---
+++ CommonDevelopProject/src/test/java/com/solutions/Solution_1461Test.java
@@ -0,0 +1,14 @@
+package com.solutions;
+
+import junit.framework.TestCase;
+
+import java.util.stream.Collectors;
+
+public class Solution_1461Test extends TestCase {
+
+    public void testGetSubString() {
+        Solution_1461 solution_1461 = new Solution_1461();
+        System.out.println(solution_1461.getSubString(3).stream().map(s -> s.replace("Start->-1->","")).collect(Collectors.toList()));
+        //System.out.println(solution_1461.getSubString(5).size());
+    }
+}
