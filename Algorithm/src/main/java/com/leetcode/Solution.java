c119152c29d3c00ebe7a6558c87383f732f6ee41
bin <huangbinme@163.com>
2021-07-01 23:02:02 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution.java
+++ Algorithm/src/main/java/com/leetcode/Solution.java
@@ -1,82 +1,22 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Solution {
-    public String[] permutation(String s) {
-        String[] strings = s.split("");
-        Arrays.sort(strings);
-        s = String.join("", strings);
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        boolean[] visit = new boolean[s.length()];
-        dfs(list, visit, sb, s, 0);
-        String[] ans = new String[list.size()];
-        return list.toArray(ans);
+    int ans = 0;
+
+    public int numWays(int n, int[][] relation, int k) {
+        dfs(n, relation, k, 0, 0);
+        return ans;
     }
 
-    private void dfs(List<String> list, boolean[] visit, StringBuilder sb, String s, int index) {
-        if (index == s.length()) {
-            list.add(sb.toString());
+    private void dfs(int n, int[][] relation, int k, int passCount, int cur) {
+        if (passCount == k) {
+            if (cur == n - 1) ans++;
             return;
         }
-        for (int i = 0; i < s.length(); i++) {
-            if (visit[i] || (i > 0 && !visit[i - 1] && s.charAt(i) == s.charAt(i - 1))) continue;
-            sb.append(s.charAt(i));
-            visit[i] = true;
-            dfs(list, visit, sb, s, index + 1);
-            sb.deleteCharAt(sb.length() - 1);
-            visit[i] = false;
+        for (int i = 0; i < relation.length; i++) {
+            if (relation[i][0] == cur) {
+                dfs(n, relation, k, passCount + 1, relation[i][1]);
+            }
         }
-    }
-
-    public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
-        ListNode slow = head, fast = head;
-        while (true) {
-            if (slow == null || fast == null || fast.next == null) return false;
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) break;
-        }
-        return true;
-    }
-
-    public ListNode detectCycle(ListNode head) {
-        if (head == null) return null;
-        ListNode slow = head, fast = head;
-        while (true) {
-            if (slow == null || fast == null || fast.next == null) return null;
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) break;
-        }
-        slow = head;
-        while (slow != fast) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return slow;
-    }
-
-    public String minNumber(int[] nums) {
-        List<String> strings = Arrays.stream(nums)
-                .boxed()
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-        Collections.sort(strings, (a, b) -> {
-            String strA = a + b;
-            String strB = b + a;
-            return strA.compareTo(strB);
-        });
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String string : strings) {
-            stringBuilder.append(string);
-        }
-        return stringBuilder.toString();
     }
 }
--- Algorithm/src/test/java/com/leetcode/SolutionTest.java
+++ Algorithm/src/test/java/com/leetcode/SolutionTest.java
@@ -7,5 +7,25 @@ public class SolutionTest {
     @Test
     public void testJump() {
         Solution solution = new Solution();
+        int[] ints1 = new int[]{0,2};
+        int[] ints2 = new int[]{2,1};
+        int[] ints3 = new int[]{3,4};
+        int[] ints4 = new int[]{2,3};
+        int[] ints5 = new int[]{1,4};
+        int[] ints6 = new int[]{2,0};
+        int[] ints7 = new int[]{0,4};
+        int[][] ints = new int[7][2];
+        ints[0] = ints1;
+        ints[1] = ints2;
+        ints[2] = ints3;
+        ints[3] = ints4;
+        ints[4] = ints5;
+        ints[5] = ints6;
+        ints[6] = ints7;
+        System.out.println(solution.numWays(5,ints,3));
+    }
+
+    @Test
+    public void testNumWays() {
     }
 }
