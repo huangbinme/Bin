716ed993b7186384d502da1ff68834055ca39575
bin <huangbinme@163.com>
2021-09-06 22:21:19 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_704.java
+++ Algorithm/src/main/java/com/leetcode/Solution_704.java
@@ -2,23 +2,18 @@ package com.leetcode;
 
 public class Solution_704 {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = getMid(left, right);
+        if (nums.length == 0) return -1;
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[left] <= target && nums[mid] > target) {
-                right = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
             } else {
-                left = mid + 1;
+                r = mid - 1;
             }
         }
-        return -1;
-    }
-
-    private int getMid(int i, int j) {
-        return i + (j - i) / 2;
+        return nums[l] == target ? l : -1;
     }
 }
