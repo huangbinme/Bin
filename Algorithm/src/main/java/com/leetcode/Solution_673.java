3b4f14d930b9d4c89b14fe9c1c4c51ea896d2c6a
bin <huangbinme@163.com>
2021-09-20 23:46:26 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_673.java
+++ Algorithm/src/main/java/com/leetcode/Solution_673.java
@@ -1,27 +1,33 @@
 package com.leetcode;
 
-import java.util.Arrays;
-
 public class Solution_673 {
     public int findNumberOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         int[] count = new int[nums.length];
-        Arrays.fill(dp, 1);
-        Arrays.fill(count, 1);
-        for (int i = 1; i < nums.length; i++) {
-            int dpValue = 1;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            count[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     if (dp[j] + 1 > dp[i]) {
-                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                        dp[i] = dp[j] + 1;
                         count[i] = count[j];
                     } else if (dp[j] + 1 == dp[i]) {
                         count[i] += count[j];
                     }
                 }
             }
-            dp[i] = dpValue;
         }
-        return 0;
+        int maxLength = dp[0];
+        for (int i = 0; i < dp.length; i++) {
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < count.length; i++) {
+            if(dp[i] == maxLength){
+                ans += count[i];
+            }
+        }
+        return ans;
     }
 }
