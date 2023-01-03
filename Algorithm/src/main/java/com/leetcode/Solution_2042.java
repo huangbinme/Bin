3e460a15157b0b8d477f6f11080d334c85e2ad0a
Bin <huangbinme@foxmail.com>
2023-01-03 21:02:34 +0800

    new solution

---
+++ Algorithm/src/main/java/com/leetcode/Solution_2042.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+public class Solution_2042 {
+    public boolean areNumbersAscending(String s) {
+        int pre = -1;
+        int l = 0, r = 0;
+        while (r < s.length()) {
+            if (s.charAt(r) - '0' >= 0 && s.charAt(r) - '0' <= 9) {
+                l = r;
+                while (r < s.length() && s.charAt(r) - '0' >= 0 && s.charAt(r) - '0' <= 9) r++;
+                int cur = Integer.parseInt(s.substring(l, r));
+                if(pre >= cur) return false;
+                pre = cur;
+                r++;
+                l = r;
+            } else {
+                r++;
+            }
+        }
+        return true;
+    }
+}
