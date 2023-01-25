4c8323c7bbbe63e65759695aeb1cc5a7df7593c0
Bin <huangbinme@foxmail.com>
2023-01-25 23:17:25 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_698.java
+++ Algorithm/src/main/java/com/leetcode/Solution_698.java
@@ -1,36 +1,34 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Solution_698 {
-    boolean ans = false;
-
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        boolean[] isSet = new boolean[nums.length];
-        List<Integer> sum = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            sum.add(0);
-        }
-        dfs(nums, isSet, 0, sum);
-        return ans;
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k != 0) return false;
+        int target = sum / k;
+        int[] ints = new int[k];
+        return dfs(nums, target, ints, 0, 0);
     }
 
-    private void dfs(int[] nums, boolean[] isSet, int count, List<Integer> sum) {
+    private boolean dfs(int[] nums, int target, int[] sum, int start, int count) {
         if (count == nums.length) {
-            ans = sum.stream().allMatch(a -> Objects.equals(a, sum.get(0)));
-            return;
+            return Arrays.stream(sum).allMatch(a -> a == sum[0]);
         }
-        for (int i = 0; i < sum.size(); i++) {
-            int curSum = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (isSet[j]) continue;
-                isSet[j] = true;
-                curSum += nums[j];
-                sum.set(i, sum.get(i) + curSum);
-
+        for (int i : sum) {
+            if (i > target) return false;
+        }
+        for (int i = start; i < nums.length; i++) {
+            int num = nums[i];
+            for (int j = 0; j < sum.length; j++) {
+                if(sum[j] + num > target) continue;
+                sum[j] += num;
+                if(sum[j] > target) continue;
+                boolean re = dfs(nums, target, sum, i + 1, count + 1);
+                sum[j] -= num;
+                if (re) return true;
             }
         }
+        return false;
     }
 }
