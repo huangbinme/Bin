8fd940259fa0da9213db82d9a2cc1f3de73880df
Bin <huangbinme@foxmail.com>
2023-02-06 22:53:15 +0800

    new solution

---
+++ Algorithm/src/main/java/com/leetcode/Solution_2331.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class Solution_2331 {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null || root.right == null) return root.val == 1;
+        if(root.val == 2){
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
