1044c29bcfa0427c762b1e02ee16c2121bfa0bde
Bin <huangbinme@foxmail.com>
2023-04-03 22:57:22 +0800

    new solution

---
+++ Algorithm/src/main/java/com/leetcode/Solution_1626.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution_1626 {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int[][] pair = new int[scores.length][2];
+        for (int i = 0; i < pair.length; i++) {
+            pair[i] = new int[]{scores[i], ages[i]};
+        }
+        Arrays.sort(pair, Comparator.<int[]>comparingInt(v -> v[0]).thenComparingInt(v -> v[1]));
+        int[] dp = new int[pair.length];
+        dp[0] = pair[0][0];
+        int ans = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pair[j][1] <= pair[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += pair[i][0];
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
---
+++ Algorithm/src/test/java/com/leetcode/Solution_1626Test.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+import org.testng.annotations.Test;
+
+public class Solution_1626Test {
+
+    @Test
+    public void testBestTeamScore() {
+        Solution_1626 solution1626 = new Solution_1626();
+        solution1626.bestTeamScore(new int[]{1, 2, 3, 5}, new int[]{8, 9, 10, 1});
+    }
+}
