b624f363298e53d5305e11da952031058376ca49
Bin <huangbinme@foxmail.com>
2024-03-30 11:49:36 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_2908.java
+++ Algorithm/src/main/java/com/leetcode/Solution_2908.java
@@ -3,18 +3,15 @@ package com.leetcode;
 public class Solution_2908 {
     public int minimumSum(int[] nums) {
         int l = nums[0], r = nums[nums.length - 1], ans = Integer.MAX_VALUE;
-        int[] minL = new int[nums.length], minR = new int[nums.length];
-        for (int i = 1; i < minL.length - 1; i++) {
-            l = Math.min(l, nums[i - 1]);
-            minL[i] = l;
-        }
+        int[] minR = new int[nums.length];
         for (int i = minR.length - 2; i > 0; i--) {
             r = Math.min(r, nums[i + 1]);
             minR[i] = r;
         }
         for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i] > minR[i] && nums[i] > minL[i]) {
-                ans = Math.min(ans, nums[i] + minR[i] + minL[i]);
+            l = Math.min(l, nums[i - 1]);
+            if (nums[i] > minR[i] && nums[i] > l) {
+                ans = Math.min(ans, nums[i] + minR[i] + l);
             }
         }
         return ans == Integer.MAX_VALUE ? -1 : ans;
