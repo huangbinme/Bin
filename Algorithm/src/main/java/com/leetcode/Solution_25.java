9d75f7f5f2322415b6c3249cee784b92b6939a97
Bin <huangbinme@foxmail.com>
2024-03-31 22:54:34 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_25.java
+++ Algorithm/src/main/java/com/leetcode/Solution_25.java
@@ -3,35 +3,41 @@ package com.leetcode;
 public class Solution_25 {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (k == 1) return head;
-        ListNode mock = new ListNode();
-        mock.next = head;
-        reverse(mock, k);
-        return mock.next;
+        ListNode dummyHead = new ListNode();
+        reverse(dummyHead, head, k);
+        return dummyHead.next;
     }
 
-    private void reverse(ListNode groupHead, int k) {
-        if (groupHead == null) return;
-        ListNode reverseHead = groupHead.next;
-        ListNode reverseTail = groupHead;
-        for (int i = 0; i < k; i++) {
-            if (reverseTail.next == null) return;
-            reverseTail = reverseTail.next;
+    private void reverse(ListNode l1, ListNode l2, int k) {
+        // Before : l1 l2 l3 l4
+        // After : l1 l3 l2 l4
+        ListNode l3 = l2;
+        for (int i = 0; i < k - 1; i++) {
+            if (l3 == null) break;
+            l3 = l3.next;
         }
-        ListNode[] reverseRes = reverse(reverseHead, reverseTail);
-        groupHead.next = reverseRes[0];
-        reverse(reverseRes[1], k);
+        if (l3 == null) return;
+        ListNode l4 = reverse(l2, l3);
+        l1.next = l3;
+        l2.next = l4;
+        reverse(l2, l4, k);
     }
 
-    private ListNode[] reverse(ListNode head, ListNode tail) {
-        ListNode pre = tail.next;
-        ListNode cur = head;
-        while (cur != tail) {
-            ListNode next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
+    private ListNode reverse(ListNode l1, ListNode l2) {
+        ListNode tail = l2.next;
+        ListNode tmp = l1.next;
+        l1.next = null;
+        reverse(l1, tmp, l2);
+        return tail;
+    }
+
+    private void reverse(ListNode l1, ListNode l2, ListNode end) {
+        if (l2 == end) {
+            l2.next = l1;
+            return;
         }
-        cur.next = pre;
-        return new ListNode[]{tail, head};
+        ListNode tmp = l2.next;
+        l2.next = l1;
+        reverse(l2, tmp, end);
     }
 }
