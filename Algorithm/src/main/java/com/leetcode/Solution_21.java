05dcab7533cf2f40d02530351a9ab722e130720b
Bin <huangbinme@foxmail.com>
2024-04-01 20:38:24 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_21.java
+++ Algorithm/src/main/java/com/leetcode/Solution_21.java
@@ -1,36 +1,20 @@
 package com.leetcode;
 
 public class Solution_21 {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        startToMerge(head, l1, l2);
-        return removeHead(head);
-    }
-
-    private ListNode removeHead(ListNode head) {
-        return head.next;
-    }
-
-    private void startToMerge(ListNode head, ListNode l1, ListNode l2) {
-        ListNode head_tmp = head;
-        ListNode l1_tmp = l1;
-        ListNode l2_tpm = l2;
-        while (l1_tmp != null && l2_tpm != null) {
-            ListNode cur = null;
-            if (l1_tmp.val <= l2_tpm.val) {
-                cur = l1_tmp;
-                l1_tmp = l1_tmp.next;
-            } else {
-                cur = l2_tpm;
-                l2_tpm = l2_tpm.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode tmp = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tmp.next = list1;
+                list1 = list1.next;
+            }else {
+                tmp.next = list2;
+                list2 = list2.next;
             }
-            head_tmp.next = cur;
-            head_tmp = head_tmp.next;
+            tmp = tmp.next;
         }
-        if (l1_tmp == null) {
-            head_tmp.next = l2_tpm;
-        } else {
-            head_tmp.next = l1_tmp;
-        }
+        tmp.next = list1 == null ? list2 : list1;
+        return dummy.next;
     }
 }
--- Algorithm/src/main/java/com/leetcode/Solution_53.java
+++ Algorithm/src/main/java/com/leetcode/Solution_53.java
@@ -2,11 +2,15 @@ package com.leetcode;
 
 public class Solution_53 {
     public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        int[] preSum = new int[nums.length + 1];
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
         }
-        return dp[dp.length - 1];
+        int ans = Integer.MIN_VALUE, min = 0;
+        for (int i = 1; i < preSum.length; i++) {
+            ans = Math.max(ans, preSum[i] - min);
+            min = Math.min(min, preSum[i]);
+        }
+        return ans;
     }
 }
