a2f3f6f8306a011f7ec86b0c046915e9bf441c33
Bin <huangbinme@foxmail.com>
2024-04-03 21:23:37 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Solution_103.java
+++ Algorithm/src/main/java/com/leetcode/Solution_103.java
@@ -3,34 +3,25 @@ package com.leetcode;
 import java.util.*;
 
 public class Solution_103 {
-
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if (root == null) return Collections.emptyList();
-        boolean startFromLeft = true;
-        int treeLevelSize = 1;
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
         Deque<TreeNode> deque = new LinkedList<>();
         deque.offerLast(root);
         while (!deque.isEmpty()) {
-            List<Integer> treeList = new ArrayList<>();
-            for (int i = 0; i < treeLevelSize; i++) {
-                TreeNode listNode;
-                if (startFromLeft) {
-                    listNode = deque.pollFirst();
-                    treeList.add(listNode.val);
-                    if (listNode.left != null) deque.offerLast(listNode.left);
-                    if (listNode.right != null) deque.offerLast(listNode.right);
-                } else {
-                    listNode = deque.pollLast();
-                    treeList.add(listNode.val);
-                    if (listNode.right != null) deque.offerFirst(listNode.right);
-                    if (listNode.left != null) deque.offerFirst(listNode.left);
-                }
+            List<Integer> sub = new ArrayList<>();
+            int size = deque.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = deque.pollFirst();
+                if (treeNode.left != null) deque.offerLast(treeNode.left);
+                if (treeNode.right != null) deque.offerLast(treeNode.right);
+                sub.add(treeNode.val);
             }
-            startFromLeft = !startFromLeft;
-            treeLevelSize = deque.size();
-            result.add(treeList);
+            ans.add(sub);
         }
-        return result;
+        for (int i = 1; i < ans.size(); i += 2) {
+            Collections.reverse(ans.get(i));
+        }
+        return ans;
     }
 }
