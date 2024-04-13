2bca02368509bba7a55848adfd68bffefe8db7bf
Bin <huangbinme@foxmail.com>
2024-04-13 14:24:44 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/Solution_2924.java
+++ Algorithm/src/main/java/com/leetcode/Solution_2924.java
@@ -1,7 +1,6 @@
 package com.leetcode;
 
 public class Solution_2924 {
-
     public int findChampion(int n, int[][] edges) {
         int[] count = new int[n];
         for (int i = 0; i < edges.length; i++) {
