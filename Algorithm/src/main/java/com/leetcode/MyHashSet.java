ab700a8ced517a11743a66ca3257df133d47e7d7
Bin <huangbinme@foxmail.com>
2024-04-14 15:20:55 +0800

    new solution

--- Algorithm/src/main/java/com/leetcode/MyHashSet.java
+++ Algorithm/src/main/java/com/leetcode/MyHashSet.java
@@ -1,22 +1,22 @@
 package com.leetcode;
 
-public class MyHashSet {
+    public class MyHashSet {
 
-    boolean[] booleans;
+        boolean[] booleans;
 
-    public MyHashSet() {
-        booleans = new boolean[1000001];
+        public MyHashSet() {
+            booleans = new boolean[1000001];
+        }
+
+        public void add(int key) {
+            booleans[key] = true;
+        }
+
+        public void remove(int key) {
+            booleans[key] = false;
+        }
+
+        public boolean contains(int key) {
+            return booleans[key];
+        }
     }
-
-    public void add(int key) {
-        booleans[key] = true;
-    }
-
-    public void remove(int key) {
-        booleans[key] = false;
-    }
-
-    public boolean contains(int key) {
-        return booleans[key];
-    }
-}
