0d34d823c9d6ff85e74b38472b4bbb4b786be9d3
Bin <huangbinme@foxmail.com>
2024-04-17 22:43:27 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Employee.java
+++ Algorithm/src/main/java/com/leetcode/Employee.java
@@ -1,5 +1,6 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Employee {
--- Algorithm/src/main/java/com/leetcode/Solution_283.java
+++ Algorithm/src/main/java/com/leetcode/Solution_283.java
@@ -2,17 +2,15 @@ package com.leetcode;
 
 public class Solution_283 {
     public void moveZeroes(int[] nums) {
-        int index = 0;
+        int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                swap(nums, i, index++);
+                if (j < i) {
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
             }
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
     }
 }
