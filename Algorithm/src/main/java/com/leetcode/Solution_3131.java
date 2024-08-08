eba4fb72b0c46d1fe7a9af509fc999e4bc7e4d1e
Bin <huangbinme@foxmail.com>
2024-08-08 22:19:25 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Solution_3131.java
+++ Algorithm/src/main/java/com/leetcode/Solution_3131.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class Solution_3131 {
     public int addedInteger(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        return nums2[0] - nums1[0];
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int i = 0; i < nums1.length; i++) {
+            min1 = Math.min(min1, nums1[i]);
+            min2 = Math.min(min2, nums2[i]);
+        }
+        return min2 - min1;
     }
 }
