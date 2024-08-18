4cafc9d0b8b5172d8e062e08b29f53e6a2c89e9a
Bin <huangbinme@foxmail.com>
2024-08-18 22:48:25 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Solution_551.java
+++ Algorithm/src/main/java/com/leetcode/Solution_551.java
@@ -2,12 +2,10 @@ package com.leetcode;
 
 public class Solution_551 {
     public boolean checkRecord(String s) {
-        int aCount = 0;
+        int countA = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'A') aCount++;
-            if ((i < s.length() - 2 && s.charAt(i) == 'L' && s.charAt(i + 2) == 'L' && s.charAt(i + 1) == 'L') || aCount >= 2)
-                return false;
+            if (s.charAt(i) == 'A') countA++;
         }
-        return true;
+        return countA < 2 && !s.contains("LLL");
     }
 }
