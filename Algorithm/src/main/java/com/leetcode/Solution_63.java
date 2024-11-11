736ec16932a66308382603851984a29475d41995
Bin <huangbinme@foxmail.com>
2024-11-11 21:13:21 +0800

    adding new solution

---
+++ Algorithm/src/main/java/com/leetcode/Solution_63.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+public class Solution_63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int[][] dp = new int[obstacleGrid.length + 1][obstacleGrid[0].length + 1];
+        dp[1][1] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (i == 1 && j == 1) continue;
+                dp[i][j] = obstacleGrid[i - 1][j - 1] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
