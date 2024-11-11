be9057f026820147735927f6c34ef3afd52b4237
Bin <huangbinme@foxmail.com>
2024-11-11 23:23:56 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Solution_743.java
+++ Algorithm/src/main/java/com/leetcode/Solution_743.java
@@ -4,34 +4,35 @@ import java.util.*;
 
 public class Solution_743 {
     public int networkDelayTime(int[][] times, int n, int k) {
+        int infinity = Integer.MAX_VALUE / 2;
         Map<Integer, List<int[]>> graph = new HashMap<>();
         for (int i = 0; i < times.length; i++) {
-            int[] time = times[i];
-            List<int[]> ints = graph.computeIfAbsent(time[0], v -> new ArrayList<>());
-            ints.add(new int[]{time[1], time[2]});
+            List<int[]> list = graph.computeIfAbsent(times[i][0], key -> new ArrayList<>());
+            list.add(new int[]{times[i][1], times[i][2]});
         }
-        boolean[] note = new boolean[n];
-        int[] minDis = new int[n];
-        Arrays.fill(minDis, Integer.MAX_VALUE / 2);
-        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(v -> v[1]));
-        queue.offer(new int[]{k, 0});
-        int ans = 0, loopCount = 0;
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        int[] minDis = new int[n + 1];
+        boolean[] loop = new boolean[n + 1];
+        Arrays.fill(minDis, infinity);
+        queue.offer(new int[]{k, infinity});
         while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int p = poll[0], dis = poll[1];
-            if (note[p - 1]) continue;
-            note[p - 1] = true;
-            loopCount++;
-            ans = Math.max(ans, dis);
-            minDis[p - 1] = dis;
-            List<int[]> child = graph.getOrDefault(p, new ArrayList<>());
-            for (int i = 0; i < child.size(); i++) {
-                int[] childP = child.get(i);
-                if (minDis[childP[0] - 1] > dis + childP[1]) {
-                    minDis[childP[0] - 1] = dis + childP[1];
-                    queue.offer(new int[]{childP[0], dis + childP[1]});
+            int[] ints = queue.poll();
+            int p = ints[0], dis = ints[1];
+            if (loop[p]) continue;
+            minDis[p] = dis;
+            loop[p] = true;
+            List<int[]> list = graph.get(p);
+            if (list != null) {
+                List<int[]> child = graph.get(p);
+                for (int[] c : child) {
+                    queue.offer(new int[]{c[0], c[1] + (dis == infinity ? 0 : dis)});
                 }
             }
+        }
+        int ans = -1, loopCount = 0;
+        for (int i = 1; i < minDis.length; i++) {
+            if (loop[i]) loopCount++;
+            if (minDis[i] != infinity) ans = Math.max(ans, minDis[i]);
         }
         return loopCount == n ? ans : -1;
     }
--- Algorithm/src/test/java/com/leetcode/Solution_743Test.java
+++ Algorithm/src/test/java/com/leetcode/Solution_743Test.java
@@ -2,16 +2,15 @@ package com.leetcode;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class Solution_743Test {
 
     @Test
     public void testNetworkDelayTime() {
         Solution_743 solution743 = new Solution_743();
-        int[][] ints = new int[2][];
+        int[][] ints = new int[3][];
         ints[0] = new int[]{1, 2, 1};
         ints[1] = new int[]{2, 3, 2};
+        ints[2] = new int[]{1, 3, 2};
         System.out.println(solution743.networkDelayTime(ints, 3, 1));
     }
 }
