e39b62b043bdd50a8e769c7edbbd8f83504144d0
Bin <huangbinme@foxmail.com>
2025-02-09 19:04:34 +0800

    adding new solution

--- Algorithm/src/main/java/com/leetcode/Solution_80.java
+++ Algorithm/src/main/java/com/leetcode/Solution_80.java
@@ -2,7 +2,14 @@ package com.leetcode;
 
 public class Solution_80 {
     public int removeDuplicates(int[] nums) {
-
+        int s = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!(s >= 2 && nums[s - 2] == nums[i])) {
+                swap(nums, s, i);
+                s++;
+            }
+        }
+        return s;
     }
 
     private void swap(int[] ints, int i, int j) {
