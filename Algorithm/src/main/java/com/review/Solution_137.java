75977f86fdb2c60a964650ffbe3eccbd924f2b71
Bin <huangbinme@foxmail.com>
2025-07-07 13:12:13 +0800

    init commit

--- Algorithm/src/main/java/com/review/Solution_137.java
+++ Algorithm/src/main/java/com/review/Solution_137.java
@@ -4,17 +4,15 @@ public class Solution_137 {
     public int singleNumber(int[] nums) {
         int[] stat = new int[32];
         for (int i = 0; i < nums.length; i++) {
-            int n = nums[i], statIndex = stat.length - 1;
+            int n = nums[i], statIndex = 0;
             while (n != 0) {
-                stat[statIndex--] += (n % 2);
+                stat[statIndex++] += (n % 2 == 0 ? 0 : 1);
                 n >>>= 1;
             }
         }
         int ans = 0;
         for (int i = 0; i < stat.length; i++) {
-            if (stat[i] % 3 != 0) {
-                ans += (1 << (31 - i));
-            }
+            ans += (stat[i] % 3 == 0 ? 0 : 1 << i);
         }
         return ans;
     }
